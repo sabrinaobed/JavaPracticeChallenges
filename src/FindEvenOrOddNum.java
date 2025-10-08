d7c6f16d24e8c9dc30d7c3512cc19c715c5ffe86
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-08 14:08:32 +0200

    Found num is odd or even

---
+++ src/FindEvenOrOddNum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FindEvenOrOddNum {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lets find if the number is Odd or Even");
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+
+        String result = evenOrOdd(num);
+        System.out.println(num + " is " + result);
+
+
+
+    }
+
+    public static String evenOrOdd(int num){
+        String result = num % 2 == 0 ? "Even" : "Odd";
+        return result;
+    }
+}
