154bc0b20ecee0eaa87df3a4628af608eda664e6
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-13 19:21:41 +0200

    used break in a loop to exit a program

---
+++ src/BreakInLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BreakInLoop {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while(true){
+            System.out.print("Enter you command: ");
+            String command = input.next();
+            if(command.equals("exit")){
+                break;
+            }
+        }
+        System.out.println("You have successfully exited!");
+    }
+}
