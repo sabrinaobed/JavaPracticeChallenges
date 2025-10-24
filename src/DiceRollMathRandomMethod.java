d2749c7eb06845d2c13e2297a038f1443e6fcc0b
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-24 10:56:31 +0200

    ddi random dice roll between 1 - 6 using Math.random method

---
+++ src/DiceRollMathRandomMethod.java
@@ -0,0 +1,7 @@
+public class DiceRollMathRandomMethod {
+    public static void main(String[] args) {
+        int diceRoll = (int)(Math.random() * 6) +1;
+
+        System.out.println("You rolled a: " + diceRoll);
+    }
+}
