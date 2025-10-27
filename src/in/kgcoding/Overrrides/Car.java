6582242d9a322ef93514d3b43245e932d1d2d7c6
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-27 11:59:26 +0100

    example of override polymorphism

---
+++ src/in/kgcoding/Overrrides/Car.java
@@ -0,0 +1,9 @@
+package in.kgcoding.Overrrides;
+
+public class Car extends Vehicle{
+    @Override
+    public void service() {
+        super.service();
+        System.out.println("Car is getting serviced..");
+    }
+}
---
+++ src/in/kgcoding/Overrrides/Vehicle.java
@@ -0,0 +1,8 @@
+package in.kgcoding.Overrrides;
+
+public class Vehicle {
+
+    public void service(){
+        System.out.println("Vehicle is getting serviced");
+    }
+}
