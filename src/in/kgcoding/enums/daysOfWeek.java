12c5cc6b416bd39568d88708ee5e340f1fee6593
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-29 13:43:26 +0100

    created a method on enum example to show day is a weekday or weekend

--- src/in/kgcoding/enums/daysOfWeek.java
+++ src/in/kgcoding/enums/daysOfWeek.java
@@ -1,11 +1,21 @@
 package in.kgcoding.enums;
 
 public enum daysOfWeek {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+    MONDAY(true),
+    TUESDAY(true),
+    WEDNESDAY(true),
+    THURSDAY(true),
+    FRIDAY(true),
+    SATURDAY(false),
+    SUNDAY(false);
+
+    private final boolean isWeekday;
+
+    daysOfWeek(boolean isWeekday) {
+        this.isWeekday = isWeekday;
+    }
+
+    public String getType(){
+        return isWeekday ? "Weekday" : "Weekend";
+    }
 }
--- src/in/kgcoding/enums/enumTest.java
+++ src/in/kgcoding/enums/enumTest.java
@@ -5,8 +5,10 @@ public class enumTest {
         System.out.println("Printing all days of the week: ");
 
         for (daysOfWeek day : daysOfWeek.values()) {
-            System.out.println(day);
+            //System.out.println(day);
+            System.out.printf("%s : %s\n" ,day,day.getType());
         }
+
 
     }
 }
