df2883d9e80c16e643de5813724fba6c08d1750f
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-29 11:45:13 +0100

    example of swapping elements in a list using set method

---
+++ src/in/kgcoding/ListSwapping/ListSwappingExample.java
@@ -0,0 +1,19 @@
+package in.kgcoding.ListSwapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListSwappingExample {
+    public static void main(String[] args) {
+        List<Integer>list = Arrays.asList(1,2,3,4,5,6,7,8);
+        System.out.println(list);
+        swap(list,2,6);
+        System.out.println(list);
+    }
+
+    public static void swap(List<Integer>list, int x, int y){
+     int swap = list.get(x);
+     list.set(x,list.get(y));
+     list.set(y,swap);
+    }
+}
