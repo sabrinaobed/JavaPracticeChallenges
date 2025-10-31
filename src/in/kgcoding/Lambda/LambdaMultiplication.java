5a69678459d4bd1b4230f306a1c8d293eee7ac57
Sabrina Obed Gill <sabrina.yousaf31@gmail.com>
2025-10-31 12:49:14 +0100

    example of lambda stored in variable through its interface

---
+++ src/in/kgcoding/Lambda/LambdaMultiplication.java
@@ -0,0 +1,12 @@
+package in.kgcoding.Lambda;
+
+import java.util.function.BinaryOperator;
+
+public class LambdaMultiplication {
+    public static void main(String[] args) {
+        BinaryOperator<Integer>multi = (a,b) ->  a * b;
+
+        int result = multi.apply(4,5);
+        System.out.println(result);
+    }
+}
